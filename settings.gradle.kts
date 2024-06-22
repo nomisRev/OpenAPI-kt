cdf85e7c61e604a9a4cd5689ab0e18f3d28924d7
Simon Vergauwen <nomisRev@users.noreply.github.com>
2024-06-22 09:59:25 +0200

    Enable scan, and --full-stacktrace

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,14 @@ dependencyResolutionManagement {
 
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+  id("com.gradle.enterprise") version "3.16.2"
+}
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
 }
 
 include("parser")
