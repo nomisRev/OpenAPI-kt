3d7edd155e7eb7d8571fb9360feaba134fb1d2f2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-06-12 11:44:48 +0200

    Add Karma configuration for setting mocha test timeout

---
+++ parser/karma.config.d/config.js
@@ -0,0 +1,8 @@
+config.set({
+    client: {
+        mocha: {
+            // We put a large timeout here so we can adjust it in the tests themselves.
+            timeout: 60000
+        }
+    }
+});
